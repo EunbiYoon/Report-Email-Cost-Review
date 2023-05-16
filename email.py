17cc8a9aa1b44f11c1ac70456d4a57412a89ab2e
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-05-16 09:11:40 -0500

    r

--- email.py
+++ email.py
@@ -1,4 +1,20 @@
-from table import *
+import pandas as pd
+import smtplib
+
+#html - table
+server = smtplib.SMTP('lgekrhqmh01.lge.com:25')
+server.ehlo()
+
+
+#메일 내용 구성
+msg=MIMEMultipart()
+
+# 수신자 발신자 지정
+msg['From']='eunbi1.yoon@lge.com'
+msg['To']='iggeun.kwon@lge.com, sehee.aiello@lge.com, jacey.jung@lge.com, gilnam.lee@lge.com, steven.yang@lge.com, jajoon1.koo@lge.com, wolyong.ha@lge.com, dowan.han@lge.com'
+msg['Cc']='ethan.son@lge.com, jongseop.kim@lge.com, richard.song@lge.com, minhyoung.sun@lge.com, kitae3.park@lge.com, tg.kim@lge.com'
+msg['Bcc']='eunbi1.yoon@lge.com'
+
 
 print(BPAE_Merge)
 print(PACE_Merge)
--- table.py
+++ table.py
@@ -5,14 +5,18 @@ import datetime
 #This weeknum
 this_week="May 3rd Week"
 
+############################ Front Loader ############################  
 #read original data
-original_BPAE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/BPAE_0512.xlsx", sheet_name="FL")
-original_PACE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/PACE_0512.xlsx", sheet_name="FL")
+F_original_BPAE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/BPAE_0512.xlsx", sheet_name="FL")
+F_original_PACE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/PACE_0512.xlsx", sheet_name="FL")
+T_original_BPAE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/BPAE_0512.xlsx", sheet_name="TL")
+T_original_PACE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/PACE_0512.xlsx", sheet_name="TL")
+D_original_BPAE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/BPAE_0512.xlsx", sheet_name="DR")
+D_original_PACE=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/PACE_0512.xlsx", sheet_name="DR")
 
 #read new data
 bpa_entity=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0519/BPA_Entity.xlsx")
 pac_entity=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0519/PAC_Entity.xlsx")
-
 
 #remain required  new data
 BPAE=bpa_entity[["Model.Suffix","Confirmed\nRMC\nWith Rate\n(USD)"]]
@@ -24,14 +28,46 @@ pac_entity.columns=pac_entity.iloc[15]
 pac_entity=pac_entity.drop([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],axis=0)
 pac_entity.reset_index(drop=True, inplace=True)
 PACE=pac_entity[["Model.Suffix","Confirmed\nRMC\nWith Rate\n(USD)"]]
-print(PACE)
 PACE["Confirmed\nRMC\nWith Rate\n(USD)"]=PACE["Confirmed\nRMC\nWith Rate\n(USD)"].astype(float).round(1)
+
 
 #Matching column with original data
 BPAE=BPAE.rename(columns={"Model.Suffix":"Tool","Confirmed\nRMC\nWith Rate\n(USD)":this_week})
 PACE=PACE.rename(columns={"Model.Suffix":"Tool","Confirmed\nRMC\nWith Rate\n(USD)":this_week})
 
-#merge two file with Model Suffix
-BPAE_Merge=pd.merge(original_BPAE,BPAE,how="inner",on="Tool")
-PACE_Merge=pd.merge(original_PACE,PACE,how="inner",on="Tool")
 
+#merge two file with Model Suffix
+F_BPAE_Merge=pd.merge(F_original_BPAE,BPAE,how="inner",on="Tool")
+F_PACE_Merge=pd.merge(F_original_PACE,PACE,how="inner",on="Tool")
+
+T_BPAE_Merge=pd.merge(T_original_BPAE,BPAE,how="inner",on="Tool")
+T_PACE_Merge=pd.merge(T_original_PACE,PACE,how="inner",on="Tool")
+
+D_BPAE_Merge=pd.merge(D_original_BPAE,BPAE,how="inner",on="Tool")
+D_PACE_Merge=pd.merge(D_original_PACE,PACE,how="inner",on="Tool")
+
+
+#set index 
+F_BPAE_Merge.index=F_BPAE_Merge["Tool"]
+F_PACE_Merge.index=F_PACE_Merge["Tool"]
+
+T_BPAE_Merge.index=T_BPAE_Merge["Tool"]
+T_PACE_Merge.index=T_PACE_Merge["Tool"]
+
+D_BPAE_Merge.index=D_BPAE_Merge["Tool"]
+D_PACE_Merge.index=D_PACE_Merge["Tool"]
+
+
+#write excel
+bpae_writer = pd.ExcelWriter("C:/Users/RnD Workstation/Documents/CostReview/0519/BPAE_0519.xlsx", engine="xlsxwriter")
+pace_writer = pd.ExcelWriter("C:/Users/RnD Workstation/Documents/CostReview/0519/PACE_0519.xlsx", engine="xlsxwriter")
+
+F_BPAE_Merge.to_excel(bpae_writer, sheet_name="FL")
+T_BPAE_Merge.to_excel(bpae_writer, sheet_name="TL")
+D_BPAE_Merge.to_excel(bpae_writer, sheet_name="DR")
+bpae_writer.close()
+
+F_PACE_Merge.to_excel(pace_writer, sheet_name="FL")
+T_PACE_Merge.to_excel(pace_writer, sheet_name="TL")
+D_PACE_Merge.to_excel(pace_writer, sheet_name="DR")
+pace_writer.close()
